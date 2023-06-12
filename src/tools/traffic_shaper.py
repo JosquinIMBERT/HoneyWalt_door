e9c20a76560e3ccc3bd4ba5cd5ec3243098ca486
Josquin IMBERT <josquin.imbert@gmail.com>
2023-06-12 10:24:22 +0200

    Fixing os import

--- src/tools/traffic_shaper.py
+++ src/tools/traffic_shaper.py
@@ -1,5 +1,5 @@
 # External
-import select, socket, sys, threading
+import os, select, socket, sys, threading
 
 if __name__ == "__main__":
 	sys.path[0] = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
