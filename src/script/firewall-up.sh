8835ccf84fd5f30f224550df441f3acc1199abb9
Josquin IMBERT <josquin.imbert@gmail.com>
2023-06-13 15:36:05 +0200

    Changing Firewall rules

--- src/script/firewall-up.sh
+++ src/script/firewall-up.sh
@@ -53,8 +53,19 @@ iptables -t mangle -P POSTROUTING ACCEPT
 ###    FIREWALL    ###
 ######################
 
-iptables -A PREROUTING -t mangle -i lo -j ACCEPT # Accept local traffic
-iptables -A PREROUTING -t mangle -p tcp	-m state --state ESTABLISHED,RELATED -j ACCEPT # Accept established connections
-iptables -A PREROUTING -t mangle -s $controller_ip -j ACCEPT # Accept controller traffic
-iptables -A PREROUTING -t mangle -p tcp --dport 22 # Accept attackers connections
-iptables -t mangle -P PREROUTING DROP # Drop everything else
+iptables -A INPUT -t filter -i lo -j ACCEPT # Accept local traffic
+iptables -A INPUT -t filter -p tcp -m state --state ESTABLISHED,RELATED -j ACCEPT # Accept established connections
+iptables -A INPUT -t filter -s ${controller_ip} -j ACCEPT # Accept controller traffic
+iptables -A INPUT -t filter -p tcp --dport 22 # Accept attackers connections
+iptables -A INPUT -t filter -p udp -i wg-srv --sport 1024:65535 --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT # Accept DNS requests
+iptables -A INPUT -t filter -p udp --sport 53 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT # Accept the answers to DNS requests
+iptables -A INPUT -t filter -p icmp -m state --state ESTABLISHED -j ACCEPT # Accept ping replies
+iptables -t filter -P INPUT DROP # Drop everything else
+
+#iptables -A PREROUTING -t mangle -i lo -j ACCEPT # Accept local traffic
+#iptables -A PREROUTING -t mangle -p tcp -m state --state ESTABLISHED,RELATED -j ACCEPT # Accept established connections
+#iptables -A PREROUTING -t mangle -s ${controller_ip} -j ACCEPT # Accept controller traffic
+#iptables -A PREROUTING -t mangle -p tcp --dport 22 # Accept attackers connections
+#iptables -A PREROUTING -t mangle -p udp -i wg-srv --sport 1024:65535 --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT # Accept DNS requests
+#iptables -A PREROUTING -t mangle -p udp --sport 53 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT # Accept the answers to DNS requests
+#iptables -t mangle -P PREROUTING DROP # Drop everything else
