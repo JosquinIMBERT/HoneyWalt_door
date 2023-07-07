71d46af1ebaaef05ccdc48abe23009cf77c2eb20
Josquin IMBERT <josquin.imbert@gmail.com>
2023-07-07 16:33:59 +0200

    Fixing door firewall

--- src/script/firewall-up.sh
+++ src/script/firewall-up.sh
@@ -62,7 +62,7 @@ iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 22 -j REDIRECT --to-port 22
 iptables -A INPUT -t filter -i lo -j ACCEPT # Accept local traffic
 iptables -A INPUT -t filter -p tcp -m state --state ESTABLISHED,RELATED -j ACCEPT # Accept established connections
 iptables -A INPUT -t filter -s ${white_list} -j ACCEPT # Accept controller traffic
-iptables -A INPUT -t filter -p tcp --dport 22 # Accept attackers connections
+iptables -A INPUT -t filter -p tcp --dport 2222 -j ACCEPT # Accept attackers connections
 iptables -A INPUT -t filter -p udp -i wg-srv --sport 1024:65535 --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT # Accept DNS requests
 iptables -A INPUT -t filter -p udp --sport 53 --dport 1024:65535 -m state --state ESTABLISHED -j ACCEPT # Accept DNS replies
 iptables -A INPUT -t filter -p icmp -m state --state ESTABLISHED -j ACCEPT # Accept ping replies
